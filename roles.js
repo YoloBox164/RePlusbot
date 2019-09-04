a6bafc7757e3b7d959394751d36f23afa766e752
CsiPA0723 <csizipeti0723@gmail.com>
2019-09-04 22:44:43 +0200

    Update

--- roles.js
+++ roles.js
@@ -1,16 +1,24 @@
 module.exports.CheckModes = (message, command) => {
     if(command === "clearmodes") {
-        message.member.removeRole('611691601990909953');
-        message.member.removeRole('611682216648507544');
+        message.member.removeRole(this.roles.nsfw);
+        message.member.removeRole(this.roles.safe);
+        message.member.addRole(this.roles.default);
         message.channel.send("Cleared all modes.");
     } else if(command === `nsfwmode`) {
-        message.member.addRole('611691601990909953');
-        message.member.removeRole('611682216648507544');
+        message.member.addRole(this.roles.nsfw);
+        message.member.removeRole(this.roles.safe);
         message.channel.send("Switched to nsfw mode.");
     } else if(command === `safemode`) {
-        message.member.addRole('611682216648507544');
-        message.member.removeRole('611691601990909953');
+        message.member.addRole(this.roles.safe);
+        message.member.removeRole(this.roles.nsfw);
+        message.member.removeRole(this.roles.default)
         message.channel.send("Switched to safe mode.");
     } else return false;
     return true;
+}
+
+module.exports.roles = {
+    default: "611682388631617544",
+    nsfw: "611691601990909953",
+    safe: "611682216648507544"
 }
