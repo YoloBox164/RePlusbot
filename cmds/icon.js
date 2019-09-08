b997a39cdd7840f696beb3a9425db832407c544f
CsiPA0723 <csizipeti0723@gmail.com>
2019-09-08 22:19:33 +0200

    init

---
+++ cmds/icon.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+module.exports.run = (bot, message, args) => {
+    var msg = await message.channel.send("Getting server icon");
+
+    var embed = new Discord.RichEmbed()
+        .setTitle("Server Icon")
+        .setDescription(`[Icon LINK](${message.guild.iconUrl})`)
+        .setImage(message.guild.iconUrl)
+        .setColor(message.guild.member(target).displayHexColor);
+
+    await message.channel.send({embed: embed});
+    msg.delete();
+}
+
+module.exports.help = {
+    cmd: "icon",
+    alias: ["servericon", "ico"],
+    name: "Server Icon",
+    desc: "Shows the server's icon",
+    usage: ">icon",
+    category: "user"
+}
