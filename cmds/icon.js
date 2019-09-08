92999688c465948f6940b5a66237685667f85142
CsiPA0723 <csizipeti0723@gmail.com>
2019-09-08 22:25:24 +0200

    bug fix

--- cmds/icon.js
+++ cmds/icon.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = (bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
     var msg = await message.channel.send("Getting server icon");
 
     var embed = new Discord.RichEmbed()
