d2c895f76f1af511731cdd6b132084a45529b730
CsiPA0723 <csizipeti0723@gmail.com>
2019-09-08 22:40:10 +0200

    init

---
+++ cmds/botinfo.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+
+const Functions = require('../functions.js');
+
+module.exports.run = async (bot, message, args) => {
+    var msg = await message.channel.send("Generating...");
+
+    var embed = new Discord.RichEmbed()
+        .setAuthor(bot.user.username)
+        .setTitle("Bot information:")
+        .setDescription(
+            `**Full Username:** *${bot.user.username}#${bot.user.discriminator}*
+            **ID:** *${bot.user.id}*\n
+            **Status:** *${bot.user.presence.status}*
+            **Created At:** *${bot.user.createdAt}*\n
+            **Creator:** *${message.guild.member("333324517730680842") || "CsiPA0723#0423"}*
+            **Guild Count:** *${bot.guilds.size}*
+            **Channel Count:** *${bot.channels.size}*
+            **User Count:** *${bot.users.size}*\n
+            **Uptime:** *${Functions.ParseMillisecondsIntoReadableTime(bot.uptime)}*`
+        )
+        .setThumbnail(bot.user.displayAvatarURL)
+        .setColor(message.guild.member(bot.user).displayHexColor);
+
+    message.channel.send({embed: embed});
+    msg.delete();
+}
+
+module.exports.help = {
+    cmd: "botinfo",
+    alais: ["bot"],
+    name: "Bot Information",
+    desc: "List of information of the bot.",
+    usage: ">botinfo",
+    category: "user"
+}
