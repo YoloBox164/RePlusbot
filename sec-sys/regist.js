f32a4fbd86fbfc5431003564c8746042c50dc67e
Péter Csizmadia <csizipeti0723@gmail.com>
2020-06-16 12:49:34 +0200

    fix for the messges.filter not being a func

--- sec-sys/regist.js
+++ sec-sys/regist.js
@@ -10,7 +10,7 @@ module.exports = {
         if(message.channel.id != Settings.registChannelId) return;
         if(message.member.roles.size > 1) return; //Has roles
 
-        if(message.channel.messages.filter(m => m.author.id == message.author.id).size > 1) {
+        if(message.channel.messages.cache.filter(m => m.author.id == message.author.id).size > 1) {
             message.author.send(`Nem lehet 1-nél több üzeneted a regisztráció szobában. Vagy töröld ki az előzőt vagy szerkezzd meg.`);
             if(message.deletable) message.delete();
             else message.guild.member("333324517730680842").send(`I could not delete this message: ${message.url}`);
