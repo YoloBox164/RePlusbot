c4b6ddf0e07cfdad4b4940a25f69cd095652ef00
Péter Csizmadia <csizipeti0723@gmail.com>
2020-12-18 00:05:50 +0100

    added cooldown

--- src/event-handlers/presenceUpdate.ts
+++ src/event-handlers/presenceUpdate.ts
@@ -1,6 +1,12 @@
-import { Presence } from "discord.js";
+import { Collection, Presence, Snowflake } from "discord.js";
 import Economy from "../systems/economy";
 
+const cooldown = new Collection<Snowflake, NodeJS.Timeout>();
+
 export default (oldPresence: Presence | null, newPresence: Presence) => {
-    Economy.CheckWumpus(newPresence.member);
+    if(!cooldown.has(newPresence.member.id)) Economy.CheckWumpus(newPresence.member).then(() => {
+        cooldown.set(newPresence.member.id, setTimeout(() => {
+            cooldown.delete(newPresence.member.id)
+        }, 3600000)); // 1 hour cooldown
+    });
 }
