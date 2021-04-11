0ecd0293e40f220df66dee1202d23301f1b994ec
Péter Csizmadia <csizipeti0723@gmail.com>
2021-04-11 15:53:51 +0200

    added youtube regex pattern

--- src/utils/regexp-patterns.ts
+++ src/utils/regexp-patterns.ts
@@ -1,5 +1,6 @@
 export default {
     GuildEmojiId: /<:\w{2,}:(\d+)>/g,
     DiscordMsgURL: /https:\/\/discordapp\.com\/channels\/(?<guildId>\d+)\/(?<channelId>\d+)\/(?<messageId>\d+)/g,
-    LinkFinder: /\b(?<!@)(?:(?!\d+)(?:(?<Protocol>\w+):\/\/)?(?<FullDomain>(?:(?:(?<www>w{0,3})\.)?(?<Domain>[\w-.]+)\.(?!\d+)(?<TLD>\w{2,3}))|(?:(?:\d{1,3})\.(?:\d{1,3})\.(?:\d{1,3})\.(?:\d{1,3})(?::?\d{1,6})))){1,68}(?<AfterString>\/[\w./-]*)?\b/gi
+    LinkFinder: /\b(?<!@)(?:(?!\d+)(?:(?<Protocol>\w+):\/\/)?(?<FullDomain>(?:(?:(?<www>w{0,3})\.)?(?<Domain>[\w-.]+)\.(?!\d+)(?<TLD>\w{2,3}))|(?:(?:\d{1,3})\.(?:\d{1,3})\.(?:\d{1,3})\.(?:\d{1,3})(?::?\d{1,6})))){1,68}(?<AfterString>\/[\w./-]*)?\b/gi,
+    Youtube:/(?:https?:\/\/)?(?:(?:(?:www\.?)?youtube\.com(?:\/(?:(?:watch\?.*?(v=[^&\s]+).*)|(?:v(\/.*))|(channel\/.+)|(?:user\/(.+))|(?:results\?(search_query=.+))))?)|(?:youtu\.be(\/.*)?))/g
 };
