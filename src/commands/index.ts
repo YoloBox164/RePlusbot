6bc158f4008e5555b2239bb65d30d955499d8157
Péter Csizmadia <csizipeti0723@gmail.com>
2021-06-16 20:20:37 +0200

    added new cmds

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,4 +1,8 @@
+export * from "./info/avatar";
+export * from "./info/botinfo";
+export * from "./info/icon";
 export * from "./info/ping";
+export * from "./info/serverinfo";
 
 export * from "./fun/cat";
 export * from "./fun/dog";
