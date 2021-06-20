9e91c4aab416e4ba348f0c6e99e9350b82139735
Péter Csizmadia <csizipeti0723@gmail.com>
2021-06-21 01:02:14 +0200

    custom discord.js structures

---
+++ src/structures/message.ts
@@ -0,0 +1,27 @@
+import { Client, DMChannel, NewsChannel, Structures, TextChannel } from "discord.js";
+import { Prefix } from "../settings";
+
+declare module "discord.js" {
+  interface Message {
+    startsWithPrefix: boolean;
+  }
+}
+
+const prefixRegex = new RegExp(`^${escapeRegExp(Prefix)}\\w+`);
+
+function escapeRegExp(string: string): string {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+}
+
+Structures.extend("Message", Message => {
+  class CustomMessage extends Message {
+
+    public startsWithPrefix = prefixRegex.test(this.content);
+
+    constructor(client: Client, data: Record<string, unknown>, channel: DMChannel | TextChannel | NewsChannel) {
+      super(client, data, channel);
+    }
+  }
+
+  return CustomMessage;
+});
---
+++ src/structures/user.ts
@@ -0,0 +1,20 @@
+import { Client, Structures } from "discord.js";
+import { DevID } from "../settings";
+
+declare module "discord.js" {
+  interface User {
+    isDev: boolean;
+  }
+}
+
+Structures.extend("User", User => {
+  class CustomUser extends User {
+    public isDev = this.id === DevID;
+
+    constructor(client: Client, data: Record<string, unknown>) {
+      super(client, data);
+    }
+  }
+
+  return CustomUser;
+});
