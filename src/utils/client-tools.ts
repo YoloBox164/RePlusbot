fa140272f3ac3e20249a6c6c730015e76ad62c89
Péter Csizmadia <csizipeti0723@gmail.com>
2021-06-21 01:02:33 +0200

    moved from client.ts

---
+++ src/utils/client-tools.ts
@@ -0,0 +1,7 @@
+import { DateTime } from "luxon";
+
+export default {
+  logDate(timestamp: number = Date.now()): string {
+    return DateTime.fromMillis(timestamp).toFormat("yyyy-MM-dd | TT 'GMT'ZZZ");
+  }
+};
