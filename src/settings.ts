6bcb3587465e6eab12a5745373275b9263e28241
Péter Csizmadia <csizipeti0723@gmail.com>
2021-06-21 01:03:34 +0200

    mulitple exports

--- src/settings.ts
+++ src/settings.ts
@@ -1,53 +1,58 @@
-const Settings = {
-  "Prefix": ">",
-  "Roles": {
-    "AutoMemberId": "818610216647524393",
-    "AutoBotId": "null",
-    "AutoSeparator": "null",
-    "MuteId": "818624862549180436",
-    "TicketId": "null",
-    "WumpusId": "null"
-  },
-  "Channels": {
-    "automodLogId": "818642199910547466",
-    "defaultChatId": "818612821959901194",
-    "economyLogId": "772239801716965377",
-    "modLogId": "818642252129763328",
-    "registId": "null",
-    "radioId": "828317710147518504",
-    "upvoteIds": [
-      "818614297616973895",
-      "818624700531867658",
-      "818614365687119942"
-    ],
-    "welcomeMsgId": "818612821959901194"
-  },
-  "Emojis": {
-    "voteupId": "709732743755595787",
-    "votedownId": "709732732678307840",
-    "ticket": "🎟️",
-    "one": "1️⃣",
-    "two": "2️⃣",
-    "three": "3️⃣",
-    "four": "4️⃣",
-    "five": "5️⃣",
-    "six": "6️⃣",
-    "seven": "7️⃣",
-    "eight": "8️⃣",
-    "nine": "9️⃣",
-    "ten": "🔟"
-  },
-  "StaffIds": [
-    "827993127394476072",
-    "818624852868464651",
-    "818607823910338652"
-  ]
+export const Channels = {
+  "automodLogId": "818642199910547466",
+  "defaultChatId": "818612821959901194",
+  "economyLogId": "772239801716965377",
+  "modLogId": "818642252129763328",
+  "registId": "null",
+  "radioId": "828317710147518504",
+  "upvoteIds": [
+    "818614297616973895",
+    "818624700531867658",
+    "818614365687119942"
+  ],
+  "welcomeMsgId": "818612821959901194"
 };
 
-export const Channels = Settings.Channels;
-export const Emojis = Settings.Emojis;
-export const Prefix = Settings.Prefix;
-export const Roles = Settings.Roles;
-export const StaffIds = Settings.StaffIds;
+export const DevID = "333324517730680842";
 
-export default Settings;
+export const Emojis = {
+  "voteupId": "709732743755595787",
+  "votedownId": "709732732678307840",
+  "ticket": "🎟️",
+  "one": "1️⃣",
+  "two": "2️⃣",
+  "three": "3️⃣",
+  "four": "4️⃣",
+  "five": "5️⃣",
+  "six": "6️⃣",
+  "seven": "7️⃣",
+  "eight": "8️⃣",
+  "nine": "9️⃣",
+  "ten": "🔟"
+};
+
+export const Prefix = ">";
+
+export const Roles = {
+  "AutoMemberId": "818610216647524393",
+  "AutoBotId": "null",
+  "AutoSeparator": "null",
+  "MuteId": "818624862549180436",
+  "TicketId": "null",
+  "WumpusId": "null"
+};
+
+export const StaffIds = [
+  "827993127394476072",
+  "818624852868464651",
+  "818607823910338652"
+];
+
+export default {
+  Channels,
+  DevID,
+  Emojis,
+  Prefix,
+  Roles,
+  StaffIds
+};
