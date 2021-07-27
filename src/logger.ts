ad2dfe7260be3d22ad5f6ba6036849b0aa05f9b1
Péter Csizmadia <csizipeti0723@gmail.com>
2021-07-27 13:37:21 +0200

    deleted defaultMeta

--- src/logger.ts
+++ src/logger.ts
@@ -3,7 +3,6 @@ import { createLogger, format, transports } from "winston";
 const logger = createLogger({
   level: "info",
   format: format.json(),
-  defaultMeta: { service: "user-service" },
   transports: [
     new transports.File({ filename: "error.log", level: "error" }),
     new transports.File({ filename: "combined.log" }),
